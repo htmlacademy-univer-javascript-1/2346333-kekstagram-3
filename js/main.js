9b6d8ba9e5291f99f2c1f800725828dbedf87e5e
Daria <daria.v2906@gmail.com>
2023-03-12 16:39:32 +0300

    Больше деталей

--- js/main.js
+++ js/main.js
@@ -10,3 +10,20 @@ function returnRandomNumber(min, max) {
 function checkMaxLength(str, maxLength) {
   return str.length <= maxLength;
 }
+
+const array = Array.from({length: 25}, (_, i) => i+1);
+
+const createObject = () => {
+  const result = array[0];
+  array.shift();
+  return {
+      id: result,
+      url: `photos/${result}.jpg`,
+      descriprion: `Фотография ${result}`,
+      likes: returnRandomNumber(15, 200),
+      comments: returnRandomNumber(0, 200),
+  };
+};
+
+const countObjects = 25;
+const arrayWithObjects = Array.from({length: countObjects}, createObject);
